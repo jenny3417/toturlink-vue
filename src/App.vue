5730c917eee46edf3f9cbed5ac1ea5222a2b59e5
jenny <jenny.341748@gmail.com>
2023-08-22 16:28:42 +0800

    add123

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="I did it!" />
+      <HelloWorld msg="I did it!123" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
