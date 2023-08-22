22f3a16b13c549133d60d682713e42e95d7fd649
YuGe <uu890817@gmail.com>
2023-08-22 16:11:46 +0800

    add !!!

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup lang="js">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="I did it!" />
+      <HelloWorld msg="I did it!!!!!!!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
