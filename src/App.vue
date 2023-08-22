fadca291909e965564eca002408916a74f2e85d0
jenny <jenny.341748@gmail.com>
2023-08-22 16:51:24 +0800

    delete 1~7

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="I did it!1234567" />
+      <HelloWorld msg="I did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
