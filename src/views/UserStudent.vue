1f0088dd983f0211d2e5a573196d8a1507bcc5f6
Gong Chi <40139252@gm.nfu.edu.tw>
2023-08-24 16:02:06 +0800

    add new vue
    add UserStudent.vue
    update Righhtmenu.vue

---
+++ src/views/UserStudent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="wrap">
+
+        <div>
+            <img src="" alt="">
+            我的學習
+        </div>
+        <router-link to="/aaa">aaa</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+    
+<script setup lang='ts'>
+
+</script>
+    
+<style scope>
+.wrap {
+    padding: 100px;
+    background-color: beige;
+}
+</style>
