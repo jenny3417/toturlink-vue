d15b379e44b85df6ae8590797977fccd867cc880
jenny <jenny.341748@gmail.com>
2023-08-24 17:04:42 +0800

    修改購物車

--- src/views/Shoppingcart.vue
+++ src/views/Shoppingcart.vue
@@ -1,64 +1,76 @@
 <template>
-    <div  class="cartPage">
-    <n-space vertical>
-      <n-space>
-        <n-progress type="circle" :percentage="percentage" />
-        <n-progress type="circle" status="info" :percentage="percentage" />
-        <n-progress type="circle" status="success" :percentage="percentage" />
-        <n-progress type="circle" status="warning" :percentage="percentage" />
-        <n-progress type="circle" status="error" :percentage="percentage" />
-      </n-space>
-      <n-space>
-        <n-button @click="minus">
-          减 10%
+
+  <n-space vertical>
+    <n-steps :current="(current as number)" :status="currentStatus">
+      <n-step
+        title="購物車"
+        description="123"
+      />
+      <n-step
+        title="Let It Be"
+        description="When I find myself in times of trouble Mother Mary comes to me"
+      />
+      <n-step
+        title="Come Together"
+        description="Here come old flat top He come grooving up slowly"
+      />
+      <n-step
+        title="Something"
+        description="Something in the way she moves Attracts me like no other lover"
+      />
+    </n-steps>
+    <n-space>
+      <n-button-group>
+        <n-button @click="prev">
+          <template #icon>
+            <n-icon>
+              <md-arrow-round-back />
+            </n-icon>
+          </template>
         </n-button>
-        <n-button @click="add">
-          加 10%
+        <n-button @click="next">
+          <template #icon>
+            <n-icon>
+              <md-arrow-round-forward />
+            </n-icon>
+          </template>
         </n-button>
-      </n-space>
+      </n-button-group>
     </n-space>
-</div>
-  </template>
-  
-  <script lang="ts">
-    import { defineComponent,ref } from 'vue'
-    // import {NButton,NProgress,NSpace} from 'naive-ui'
-  
-    export default defineComponent({
-    // components: {
-    //   NButton,
-    //   NProgress,
-    //   NSpace
-    // },
-    setup () {
-    const percentageRef = ref(0)
+  </n-space>
+</template>
 
-    const add = () => {
-      percentageRef.value += 10
-      if (percentageRef.value > 100) {
-        percentageRef.value = 0;
-      }
-    }
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { MdArrowRoundBack, MdArrowRoundForward } from '@vicons/ionicons4'
 
-    const minus = () => {
-      percentageRef.value -= 10
-      if (percentageRef.value < 0) {
-        percentageRef.value = 100
-      }
-    }
-
+export default defineComponent({
+  components: {
+    MdArrowRoundBack,
+    MdArrowRoundForward
+  },
+  setup () {
+    const currentRef = ref<number | null>(1)
     return {
-      percentage: percentageRef,
-      add,
-      minus
+      currentStatus: ref<StepsProps['status']>('process'),
+      current: currentRef,
+      next () {
+        if (currentRef.value === null) currentRef.value = 1
+        else if (currentRef.value >= 4) currentRef.value = null
+        else currentRef.value++
+      },
+      prev () {
+        if (currentRef.value === 0) currentRef.value = null
+        else if (currentRef.value === null) currentRef.value = 4
+        else currentRef.value--
+      }
     }
   }
 })
-  </script>
+</script>
 
-  <style>
-.cartPage{
-
-    margin-top: 100px;
+<style>
+.n-space{
+  margin: 100px;
 }
 </style>
