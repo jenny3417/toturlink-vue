acf94035ce3a2e3ad66d0cd0c9f1f6260ad3f3ed
jenny <jenny.341748@gmail.com>
2023-08-25 15:34:48 +0800

    修改購物車

--- src/views/Shoppingcart.vue
+++ src/views/Shoppingcart.vue
@@ -1,12 +1,13 @@
 <template>
   <navbar></navbar>
-  <steps></steps>
+  <!-- <steps></steps> -->
+  <item></item>
 </template>
   
 <script setup>
-import steps from "../components/ShoppingCartSteps.vue"
+// import steps from "../components/ShoppingCartSteps.vue"
 import navbar from "../components/Navbar.vue"
-
+import item from "../components/ShoppingCartItems.vue"
 
 
 </script>
