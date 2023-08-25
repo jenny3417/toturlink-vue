80e1539de19b9a6deedc74eca043fcd562df51a4
jenny <jenny.341748@gmail.com>
2023-08-25 10:20:19 +0800

    建立新專案

---
+++ src/views/Shoppingcart.vue
@@ -0,0 +1,13 @@
+<template>
+  <navbar></navbar>
+  <steps></steps>
+</template>
+  
+<script setup>
+import steps from "../components/ShoppingCartSteps.vue"
+import navbar from "../components/Navbar.vue"
+
+
+
+</script>
+
