dccb33d600ed4c9c8f9d316dd373b9557c000d26
jenny <jenny.341748@gmail.com>
2023-08-25 17:52:17 +0800

    修理購物車

--- src/components/ShoppingCartItems.vue
+++ src/components/ShoppingCartItems.vue
@@ -1,11 +1,22 @@
-<template lang="">
-    <div>
-        <div>
-            <div>課程資訊</div>
-            <div>優惠價</div>
-            <div>數量</div>
-            <div>總計</div>
-            <div></div>
+<template>
+    <!-- <div class="item-body">
+        <div class="lesson-info">
+            <img src="https://fakeimg.pl/150x90/" alt="課程圖片">
+            <div class="lesson-name">課程名稱</div>
+        </div>
+        <div>影音課程</div>
+        <div>1</div>
+        <div>$55</div>
+    </div> -->
+    <div class="cart-header-gold cart-list-header">
+        <div class="row px-0 mx-0">
+            <div class="product-info-title col-10 col-lg-6 offset-1 offset-md-0 item-center mx-0">
+                <img src="https://fakeimg.pl/150x90/" alt="">課程資訊
+            </div>
+            <div class="col-1 item-center p-0 d-none d-lg-flex item-info">類別</div>
+            <div class="col-2 item-center p-0 d-none d-lg-flex item-info">數量</div>
+            <div class="col-1 item-center p-0 d-none d-lg-flex item-info">小計</div>
+            <div class="col-2 item-center p-0 d-none d-lg-flex item-info">x</div>
         </div>
     </div>
 </template>
@@ -15,18 +26,20 @@ export default {
 }
 </script>
 <style scoped>
-
-.html{
-    
+* {
     margin: 0;
     padding: 0;
     border: 0;
     font-size: 1rem;
     font: inherit;
-    vertical-align: baseline;
+}
+.cart-header-gold{
+  width: 80%;
+  background-color: aquamarine;
+  margin: 5px auto;
 
 }
-.cart-list-header {
-    text-align: center;
+.item-info{
+    align-items: center
 }
 </style>
--- src/views/Shoppingcart.vue
+++ src/views/Shoppingcart.vue
@@ -1,6 +1,27 @@
 <template>
   <navbar></navbar>
   <!-- <steps></steps> -->
+
+  <!-- <div class="step">
+    <h1>進度條</h1>
+  </div>
+  <div class="item-title">
+    <div class="lesson-info">課程資訊</div>
+    <div>類別</div>
+    <div>數量</div>
+    <div>小計</div>
+  </div>
+  <item></item> -->
+
+  <div class="cart-header-gold cart-list-header">
+    <div class="row px-0 mx-0">
+      <div class="product-info-title col-10 col-lg-6 offset-1 offset-md-0 item-center mx-0">課程資訊</div>
+      <div class="col-1 item-center p-0 d-none d-lg-flex">類別</div>
+      <div class="col-2 item-center p-0 d-none d-lg-flex">數量</div>
+      <div class="col-1 item-center p-0 d-none d-lg-flex">小計</div>
+      <div class="col-2 item-center p-0 d-none d-lg-flex"></div>
+    </div>
+  </div>
   <item></item>
 </template>
     
@@ -12,4 +33,19 @@ import item from "../components/ShoppingCartItems.vue"
 
 </script>
     
-<style></style>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 1rem;
+  font: inherit;
+}
+
+.cart-header-gold{
+  width: 80%;
+  background-color: aquamarine;
+  margin: 5px auto;
+}
+</style>
