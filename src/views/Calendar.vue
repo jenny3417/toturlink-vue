084fd14bfb5ade6e7bf36c24b37c7cb2be60ae53
WeiC <orz556683@gmail.com>
2023-08-28 09:53:08 +0800

    新增評分、收藏及主頁元件分類資料夾，請更新導覽列位址

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -3,13 +3,21 @@
 
     <div class="container calendarPage">
         <h1>我的行事曆</h1>
-
+        <vue-cal ref="vuecal" small hide-view-selector hide-title-bar :time-from="7 * 60" :time-to="23 * 60"
+            :disable-views="['years', 'year', 'day']" :cell-click-hold="false" :drag-to-create-event="false" editable-events
+            @cell-click="$refs.vuecal.createEvent(
+                $event,
+                60,
+                { title: '英文課', class: 'blue-event' }
+            )">
+        </vue-cal>
     </div>
 </template>
     
 <script setup>
-import navbar from "../components/Navbar.vue"
-
+import navbar from "../components/public/Navbar.vue"
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
 </script>
     
-<style></style>
+<style scoped></style>
