97fbf0b4c6a6617a565c8993c0a27f42ab5e3038
jenny <jenny.341748@gmail.com>
2023-09-05 15:49:45 +0800

    修正大小寫

--- src/router/index.js
+++ src/router/index.js
@@ -144,7 +144,7 @@ const router = createRouter({
         {
           path: "updateExercise/:id?",
           component: () =>
-            import("@/components/exercises/teachers/updateExercise.vue"),
+            import("@/components/exercises/teachers/UpdateExercise.vue"),
         },
         {
           path: "lesson/lessonInterFace",
