f70e69c54ee8b80b34bc77a383fc9328c1f38d8c
exin <ray29.liao@gamil.com>
2023-09-13 10:15:03 +0800

    更新CK編輯器 EditLesson、InsertLessonPage

--- src/views/EditLesson.vue
+++ src/views/EditLesson.vue
@@ -1,50 +1,132 @@
 <template>
     <Navbar></Navbar>
     <div class="container">
-        <div>
-            傳來的參數:{{ leesonId }}
-            <br>
-            後端送來的值:{{ lessonContent }}
+        <div class="editContainer">
+            <h2 style="text-align: center; margin-bottom: 16px;">編輯課程</h2>
+            <hr>
+            <div>
+                <div>
+                    <h4>課程名稱</h4>
+                    <input type="text" v-model="lessonName" style="width: 25%;">
+                </div>
+                <div style="margin-top: 16px;">
+                    <h4>上課網址</h4>
+                    <input type="text" v-model="meetingUrl" style="width: 25%;">
+                </div>
+                <div style="margin-top: 16px; width: 25%;">
+                    <h4>課程內容</h4>
+                    <!-- <CkEditor :editorContent="lessonContent" @emitContent="handleEditorContentUpdate"></CkEditor> -->
+                    <ckeditor :editor="editor" v-model="lessonContent" :config="editorConfig"></ckeditor>
+                </div>
+                <div style="margin-top: 16px;">
+                    <h4>價格</h4>
+                    <input type="text" v-model="price" style="width: 25%;">
+                </div>
+                <div style="position: absolute;
+                            right: 480px;
+                            top: 160px;">
+                    <h4>圖片</h4>
+                    <label for="file-input" class="upload-Image">
+                        <img v-if="uploadedImage" :src="uploadedImage" alt="upload" style="
+                        width: 400px;height: 240px;">
+                        <img v-else :src="`${str}${image}`" alt="upload" style="
+                       width: 400px;height: 240px;">
+                    </label>
+                    <input type="file" id="file-input" @change="handleImageUpload">
+                </div>
+            </div>
+            <div class="checkBtn-block">
+                <RouterLink to="/member/teacher/mylesson">
+                    <button type="button" class="checkBtn cancel">取消</button>
+                </RouterLink>
+                <RouterLink to="/member/lesson/checkEdit">
+                    <button type="button" class="checkBtn upload">預覽</button>
+                </RouterLink>
+            </div>
         </div>
-
-        <div style="margin-top: 120px;">
-            <h2 style="text-align: center; margin-bottom: 16px;">編輯課程內容</h2>
-            <CkEditor :editorContent="lessonContent" @emitContent="handleEditorContentUpdate"></CkEditor>
-        </div>
-        <div class="checkBtn-block">
-            <RouterLink to="/member/teacher/mylesson">
-                <button type="button" class="checkBtn cancel">取消</button>
-            </RouterLink>
-            <RouterLink to="/member/lesson/checkEdit">
-                <button type="button" class="checkBtn upload">預覽</button>
-            </RouterLink>
-        </div>
-
     </div>
 </template>
     
 <script setup>
-import CkEditor from '@/components/lessons/CkEditor.vue';
+// import CkEditor from '@/components/lessons/CkEditor.vue';
 import Navbar from '@/components/public/Navbar.vue';
 import { useRoute } from 'vue-router';
 import tutorlink from '@/api/tutorlink.js';
-import { ref } from 'vue';
+import { ref, onBeforeUnmount } from 'vue';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
+
+//取得課程ID
 const route = useRoute();
 const leesonId = route.params.lessonId
 
 const lessondetail = ref([]);
 let lessonContent = ref('');
+let meetingUrl = ref('')
 //取得課程詳細資料
 tutorlink.get(`/findLessonDetailByLessonId?lessonId=${leesonId}`).then((response) => {
     lessondetail.value = response.data;
-    console.log(lessondetail.value.imformation);
+    console.log(lessondetail.value);
     lessonContent.value = lessondetail.value.imformation
+    meetingUrl.value = lessondetail.value.meetingUrl
+})
+const lesson = ref([]);
+let lessonName = ref('')
+let price = ref('');
+let image = ref('');
+//讀取Base64資料的Headers
+const str = 'data:imagae/png;base64,';
+tutorlink.post(`/findLessons/${leesonId}`).then((response) => {
+    lesson.value = response.data;
+    console.log(lesson.value);
+    lessonName.value = lesson.value.lessonName
+    image.value = lesson.value.image
+    price.value = lesson.value.price
 })
 
-const handleEditorContentUpdate = (content) => {
-    lessonContent.value = content;
+//圖片新增與預覽
+const uploadedImage = ref(null); // 初始化为 null
+const uploadedImageFile = ref(null); // 初始化为 null
+
+const handleImageUpload = (event) => {
+    lesson.value.image = event.target.files[0];
+    uploadedImageFile.value = event.target.files[0]; // 存储上传的文件
+    uploadedImage.value = URL.createObjectURL(event.target.files[0]); // 显示预览图片
+}
+onBeforeUnmount(() => {
+    if (uploadedImage.value) {
+        URL.revokeObjectURL(uploadedImage.value);
+    }
+});
+
+
+
+
+
+
+
+const editor = ClassicEditor;
+const editorConfig = {
+    toolbar: {
+        items: [
+            'heading',
+            '|',
+            'bold',
+            'italic',
+            'link',
+            '|',
+            'bulletedList',
+            'numberedList',
+            '|',
+            'undo',
+            'redo',
+        ],
+    },
+    // 其他配置项
 };
+
+
+
 
 
 </script>
@@ -64,18 +146,19 @@ const handleEditorContentUpdate = (content) => {
     width: 120px;
     height: 60px;
     border-radius: 8px;
-    background-color: #fff;
+
 }
 
 .upload {
     border: 1px solid green;
-    color: green;
+    color: #fff;
+    background-color: green;
 }
 
 .upload:hover {
     border: 1px solid green;
-    color: #fff;
-    background-color: green;
+    color: green;
+    background-color: #fff;
 }
 
 .upload:active {
@@ -86,18 +169,35 @@ const handleEditorContentUpdate = (content) => {
 
 .cancel {
     border: 1px solid red;
-    color: red;
+    color: #fff;
+    background-color: red;
 }
 
 .cancel:hover {
     border: 1px solid red;
-    color: #fff;
-    background-color: red;
+    color: red;
+    background-color: #fff;
 }
 
 .cancel:active {
     border: 1px solid red;
     color: #fff;
     background-color: rgb(107, 16, 16);
+}
+
+.editContainer {
+    border: 1px solid black;
+    border-radius: 8px;
+    padding: 24px;
+}
+
+input[type="file"] {
+    display: none;
+}
+
+.upload-Image {
+    /* border: 3px solid black; */
+    display: inline-block;
+    cursor: pointer;
 }
 </style>
