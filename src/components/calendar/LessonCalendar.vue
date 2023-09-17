8f360f196a2db400041038d045c637b6706149dc
WeiC0907 <orz556683@gmail.com>
2023-09-17 12:08:23 +0800

    新增課程行事曆

--- src/components/calendar/LessonCalendar.vue
+++ src/components/calendar/LessonCalendar.vue
@@ -192,7 +192,7 @@ const calenderList = ref([])
 async function fetchData() {
     // 啟用cookie使用者
     // 1請寫入使用者ID getAllCookies()
-    await lessonCalenderAjax(2);
+    await lessonCalenderAjax(1);
     const { lessonCalender } = storeToRefs(calenderStore);
     calenderList.value = lessonCalender.value;
 }
