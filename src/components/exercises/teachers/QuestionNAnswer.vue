c663a60e7d2581add200237c01abc2951b09207f
YuGe <uu890817@gmail.com>
2023-09-18 14:07:22 +0800

    更新QA介面

--- src/components/exercises/teachers/QuestionNAnswer.vue
+++ src/components/exercises/teachers/QuestionNAnswer.vue
@@ -12,26 +12,69 @@
         未回應
     </n-button>
 
-    <div class="ansWrap">
-        <QuestionBox></QuestionBox>
-        <QuestionBox></QuestionBox>
-        <QuestionBox></QuestionBox>
-        <QuestionBox></QuestionBox>
-        <QuestionBox></QuestionBox>
+    <!-- <div class="ansWrap"> -->
+    <div v-if="showQA">
+        <div class="queWrap" v-for="questionData in questionDatas">
+            <QA :questionData="questionData" @sendAnswer="sendAnswer"></QA>
+        </div>
     </div>
+    <!-- </div> -->
+    <!-- {{ epId }}
+    {{ questionDatas }} -->
 </template>
 
 <script setup>
+import QA from '@/components/exercises/teachers/teachersComponents/Question.vue'
 import QuestionBox from '@/components/exercises/teachers/teachersComponents/QuestionBox.vue'
 import { MdCheckmarkCircle, MdCloseCircle, MdSad } from '@vicons/ionicons4'
+import { ref } from 'vue'
+import tutorlink from '@/api/tutorlink.js'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const showQA = ref(false)
+
+const questionDatas = ref([])
+const epId = ref(route.params.id);
+
+
+
+
+const sendAnswer = () => {
+    showQA.value = false
+    getQuestion()
+
+}
+
+
+
+const getQuestion = async () => {
+    let resData = await tutorlink.get(`/teacher/getAllQuestion/${epId.value}`)
+    questionDatas.value = resData.data.data
+    console.log(resData.data);
+    showQA.value = true
+}
+getQuestion()
+
+
+
+
+
 </script>
 
 <style scoped>
-/* .ansWrap {
+* {
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -o-user-select: none;
+    user-select: none;
+}
 
-    margin: 10px;
-    padding: 10px 0 0 50px;
-} */
+.queWrap {
+    margin: 10px 0;
+    padding: 0 80px 0 50px;
+}
 
 .ansWrap {
     display: flex;
