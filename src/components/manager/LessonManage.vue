479744b4c5309ec6257841863240c8720e31b671
suycci <su170806@gmail.com>
2023-09-19 10:53:31 +0800

    更新課程影片頁面、更新課程後台

--- src/components/manager/LessonManage.vue
+++ src/components/manager/LessonManage.vue
@@ -89,7 +89,7 @@
 <script setup lang="js">
 import { BookSharp } from "@vicons/ionicons5"
 import tutorlink from '@/api/tutorlink.js';
-import { ref, reactive } from 'vue';
+import { ref, reactive,onMounted } from 'vue';
 import { useRouter } from 'vue-router'
 import Paging from "../manager/Paging.vue";
 import PageSize from "../manager/PageSize.vue";
@@ -99,15 +99,15 @@ import PageSize from "../manager/PageSize.vue";
 const lessons = ref([])
 const subjectContent = ref()
 const image = ref([])
-const getAll = async()=>{tutorlink.get(`/getAllLesson`).then((response) => {
-    lessons.value = response.data
+// const getAll = async()=>{tutorlink.post(`/getAllLesson`).then((response) => {
+//     lessons.value = response.data
 
-    image.value = lessons.value.image
-    console.log('所有課程資訊', lessons.value);
+//     image.value = lessons.value.image
+//     console.log('所有課程資訊', lessons.value);
 
 
-})}
-getAll();
+// })}
+// getAll();
 
 const subjects = ref([]);
 const subjectData = ref("");
@@ -122,7 +122,7 @@ tutorlink.get("/allSubjects").then((response) => {
 const selectedSub = async(subjectId)=>{ tutorlink.get(`/getLessonBysubjectId/${subjectId}`).then((response) => {
     lessons.value = response.data
     image.value = lessons.value.image
-    console.log('所有課程資訊', lessons.value);
+    console.log('所有課程資訊',response);
 
 
 })}
@@ -138,14 +138,16 @@ const datas = reactive({
 
 
 const loadLessons = async () => {
-    const API_URL = "/allSubjects";
-    const response = await tutorlink.get(API_URL, datas);
+    const API_URL = "/getAllLesson";
+    const response = await tutorlink.post(API_URL, datas);
     //取得所有用戶放進users變數
-    users.value = response.data.user;
+    lessons.value = response.data.lesson;
     // 計算總共幾頁
     totalPages.value = +datas.rows === 0 ? 1 : Math.ceil(response.data.count / datas.rows)
-    console.log(response.data)
+    console.log("資料",response.data)
+    console.log("資料lesson",lessons.value)
     console.log("totalPages：", totalPages.value)
+    console.log(response.data.total);
 }
 
 //paging 由子元件觸發
@@ -163,6 +165,10 @@ const changeHandler = value => {
 }
 
 loadLessons();
+
+onMounted(() => {
+    loadLessons();
+});
 </script>
 
 <style scoped>
