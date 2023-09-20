4d77a4a05b548538bf967dc20682c7f0fe73e3aa
suycci <su170806@gmail.com>
2023-09-20 12:15:07 +0800

    更新課程搜尋

--- src/stores/useLessonsStore.js
+++ src/stores/useLessonsStore.js
@@ -1,40 +1,39 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
-import tutorlink from '../api/tutorlink.js'
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+import tutorlink from "../api/tutorlink.js";
 
-export const useLessonsStore = defineStore('lessonsStore', () => {
+export const useLessonsStore = defineStore("lessonsStore", () => {
+  const lessonList = ref([]);
+  const vedioList = ref([]);
+  const onlineList = ref([]);
 
-    const lessonList = ref([])
-    const vedioList = ref([])
-    const onlineList = ref([])
-
-    async function lessonsAjax() {
-        try {
-            const response = await tutorlink.get("/findAllLesson");
-            lessonList.value = response.data
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
+  async function lessonsAjax() {
+    try {
+      const response = await tutorlink.post("/findAllLesson");
+      lessonList.value = response.data;
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
+  }
 
-    async function lessonsTypeAjax() {
-        try {
-            const response = await tutorlink.get("/findAllLesson");
-            const onlineListArr = [];
-            const vedioListArr = [];
-            response.data.forEach(list => {
-                if (list.lessonType) {
-                    onlineListArr.push(list);
-                    onlineList.value = onlineListArr
-                } else {
-                    vedioListArr.push(list)
-                    vedioList.value = vedioListArr
-                }
-            });
-        } catch (error) {
-            console.error('Error fetching data:', error);
+  async function lessonsTypeAjax() {
+    try {
+      const response = await tutorlink.post("/findAllLesson");
+      const onlineListArr = [];
+      const vedioListArr = [];
+      response.data.forEach((list) => {
+        if (list.lessonType) {
+          onlineListArr.push(list);
+          onlineList.value = onlineListArr;
+        } else {
+          vedioListArr.push(list);
+          vedioList.value = vedioListArr;
         }
+      });
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
+  }
 
-    return { lessonList, vedioList, onlineList, lessonsAjax, lessonsTypeAjax }
-})
+  return { lessonList, vedioList, onlineList, lessonsAjax, lessonsTypeAjax };
+});
